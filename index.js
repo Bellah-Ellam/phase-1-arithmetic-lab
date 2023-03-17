d2f7fb39975fc8a996047b30ee06bfcc0c24136c
Bellah Ellam <bella.ellam@student.moringaschool.com>
2023-03-17 23:22:25 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
 // Write your code here
+
+let num1=2
+let num2=31
+let num3=20
+let num4=8
+
+
+let multiply= num1*num2
+console.log(multiply)
+
+let random = Math.floor(Math.random() * 10) + 1;
+console.log(random)
+
+let mod = num3%num4
+mod
+
+let max = Math.max(num1,num3,num4)
+max
